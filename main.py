0c29e78cae19eca6229452c150629f794b20e73f
Cleir Du <48302898+cleirdu@users.noreply.github.com>
2024-11-17 17:34:15 +0800

    Add a new feature.
    Users now can choose different values for seperate RGB channels
    themselves and see the resulting image.

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ import gradio as gr
 import numpy as np
 
 
-def first(image: np.ndarray) -> np.ndarray:
+def first(image: np.ndarray, r, g, b) -> np.ndarray:
     # Here you can manipulate the image
+    buffer = image.copy().astype(np.float32)
+    image[:, :, 0] = np.round(buffer[:, :, 0] * r / 100).astype(np.uint8)
+    image[:, :, 1] = np.round(buffer[:, :, 1] * g / 100).astype(np.uint8)
+    image[:, :, 2] = np.round(buffer[:, :, 2] * b / 100).astype(np.uint8)
     return image
 
 
 if __name__ == '__main__':
     gr.Interface(
         fn=first,
-        inputs=['image'],
-        outputs=['image']
+        inputs=['image',
+                gr.Slider(minimum=0,
+                          maximum=100,
+                          value=100,
+                          label='R %'),
+                gr.Slider(minimum=0,
+                          maximum=100,
+                          value=100,
+                          label='G %'),
+                gr.Slider(minimum=0,
+                          maximum=100,
+                          value=100,
+                          label='B %')],
+        outputs=['image'],
+        live=True
     ).launch()
