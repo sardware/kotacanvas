d9c60413c0dafe71317cb1d5d5334844a3c4fae9
Cristiano Pierandrei <cris87@gmail.com>
2024-11-17 17:28:38 +0800

    Face detection

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import gradio as gr
 import numpy as np
+import cv2 as cv
 
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-def first(image: np.ndarray) -> np.ndarray:
-    # Here you can manipulate the image
-    return image
+def first(image: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=6, minSize=(30, 30))
+    center_image = image
+    for (x, y, w, h) in faces:
+        cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        center_image = image[y:y+h, x:x+w]
+
+    return center_image, image
 
 
 if __name__ == '__main__':
     gr.Interface(
         fn=first,
         inputs=['image'],
-        outputs=['image']
+        outputs=['image', 'image']
     ).launch()
